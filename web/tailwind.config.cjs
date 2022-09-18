3fba2606581fa1ae0ba85cdd43aa774acc253442
Thiago <gmoliveira69@gmail.com>
2022-09-18 01:19:14 -0300

    main page layout

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
     },
 
     extend: {
+      colors: {},
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)',
